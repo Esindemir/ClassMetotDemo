9d9707577f949c19a389d52d059da6142af3c088
esndmr <esndmr@gmail.com>
2021-02-03 00:42:09 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler.Sisteme eklendiniz :" + musteri.Adi);
+        }
+        public void Ekle2(string musteriAdi , string Soyadi)
+        {
+            Console.WriteLine("Silme işleminiz gerçekleştirildi. :" + musteriAdi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Esin";
+            musteri1.Soyadi = "Demir";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Betül";
+            musteri2.Soyadi = "Demir";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(".......");
+            }
+
+            Console.WriteLine("........Metotlar........");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Ekle2("Bedriye", "Demir");
+            musteriManager.Ekle2("Behiye", "Temel");
+        }
+    }
+}
